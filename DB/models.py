0262d50a743ba4b47f83e098744f21fbbdaae618
dnwndls411 <dnwndls411@gmail.com>
2024-05-31 00:10:17 +0900

    분노, 불안, 중립, 당황 노래 테이블 삭제'

--- DB/models.py
+++ DB/models.py
@@ -17,70 +17,10 @@ class SadMusic(Base):
    embrrased = Column(DOUBLE, nullable=False)
    anxiety = Column(DOUBLE, nullable=False)
    love = Column(DOUBLE, nullable=False)
-   
-#분노
-class AngryMusic(Base):
-   __tablename__ = "angry_music"
-
-   id = Column(BIGINT, nullable=False, autoincrement=True, primary_key=True)
-   spotipyId = Column(TEXT, nullable=False)
-   title = Column(TEXT, nullable=False)
-   angry = Column(DOUBLE, nullable=False)
-   sad = Column(DOUBLE, nullable=False)
-   delight = Column(DOUBLE, nullable=False)
-   calm = Column(DOUBLE, nullable=False)
-   embrrased = Column(DOUBLE, nullable=False)
-   anxiety = Column(DOUBLE, nullable=False)
-   love = Column(DOUBLE, nullable=False)
 
 #기쁨
 class DelightMusic(Base):
    __tablename__ = "delight_music"
-
-   id = Column(BIGINT, nullable=False, autoincrement=True, primary_key=True)
-   spotipyId = Column(TEXT, nullable=False)
-   title = Column(TEXT, nullable=False)
-   angry = Column(DOUBLE, nullable=False)
-   sad = Column(DOUBLE, nullable=False)
-   delight = Column(DOUBLE, nullable=False)
-   calm = Column(DOUBLE, nullable=False)
-   embrrased = Column(DOUBLE, nullable=False)
-   anxiety = Column(DOUBLE, nullable=False)
-   love = Column(DOUBLE, nullable=False)
-
-#중립
-class CalmMusic(Base):
-   __tablename__ = "calm_music"
-
-   id = Column(BIGINT, nullable=False, autoincrement=True, primary_key=True)
-   spotipyId = Column(TEXT, nullable=False)
-   title = Column(TEXT, nullable=False)
-   angry = Column(DOUBLE, nullable=False)
-   sad = Column(DOUBLE, nullable=False)
-   delight = Column(DOUBLE, nullable=False)
-   calm = Column(DOUBLE, nullable=False)
-   embrrased = Column(DOUBLE, nullable=False)
-   anxiety = Column(DOUBLE, nullable=False)
-   love = Column(DOUBLE, nullable=False)
-
-#당황
-class EmbrrasedMusic(Base):
-   __tablename__ = "embrrased_music"
-
-   id = Column(BIGINT, nullable=False, autoincrement=True, primary_key=True)
-   spotipyId = Column(TEXT, nullable=False)
-   title = Column(TEXT, nullable=False)
-   angry = Column(DOUBLE, nullable=False)
-   sad = Column(DOUBLE, nullable=False)
-   delight = Column(DOUBLE, nullable=False)
-   calm = Column(DOUBLE, nullable=False)
-   embrrased = Column(DOUBLE, nullable=False)
-   anxiety = Column(DOUBLE, nullable=False)
-   love = Column(DOUBLE, nullable=False)
- 
-#불안  
-class AnxietyMusic(Base):
-   __tablename__ = "anxiety_music"
 
    id = Column(BIGINT, nullable=False, autoincrement=True, primary_key=True)
    spotipyId = Column(TEXT, nullable=False)
